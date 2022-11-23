1cd37972d128f18ccc6dc10dcd777c017b5c297f
thilak <you@domain.com>
2022-11-23 19:24:52 +0530

    module1 initial commit

---
+++ module01-practice-question01/src/main/java/com/spring/professional/exam/tutorial/module01/question01/no/dependency/injection/Runner.java
@@ -0,0 +1,11 @@
+package com.spring.professional.exam.tutorial.module01.question01.no.dependency.injection;
+
+public class Runner {
+    public static void main(String... args) {
+        EmployeesSalariesReportService employeesSalariesReportService = new EmployeesSalariesReportService();
+
+        //employeesSalariesReportService.generateReport();
+        employeesSalariesReportService.generateReport1();
+
+    }
+}
